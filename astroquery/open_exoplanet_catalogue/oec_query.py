e7d138d3997cf692a0aceec12fc31060b5461eb9
CadenArmstrong <cadenarmstrong@gmail.com>
2014-05-12 16:40:42 -0400

    Changed OEC_SERVER and OEC_META_SERVER to be string variables. Added methods to change queries from remote to local.

--- astroquery/open_exoplanet_catalogue/oec_query.py
+++ astroquery/open_exoplanet_catalogue/oec_query.py
@@ -15,23 +15,50 @@ The function query_planet_to_obj returns a System object of a planet as defined 
 :Author: Hanno Rein (hanno@hanno-rein.de)
 """
 from __future__ import print_function
-from . import OEC_SERVER
-from . import OEC_META_SERVER
 from oec2py import xml_to_obj
 import sys
 import urllib2
 import csv
 import xml.etree.ElementTree as ET
+from astropy.config import ConfigurationItem
 
 
 class CatalogueOpeningError(Exception):
     """Exception for error in opening open exoplanet catalogue"""
     pass
 
-__all__ = ['query_system_xml','query_planet', 'query_system_to_obj',\
-        'query_planet_to_obj']
+__all__ = ['query_system_xml','query_planet_obj','query_planet_xml', 'query_planet_dict', 'query_system_obj',\
+        'query_system_dict', 'all_system_identifiers','all_planet_identifiers','all_star_identifiers', 'change_to_local', 'change_to_remote']
 
 aliases = None
+OEC_SERVER = 'https://raw.github.com/hannorein/open_exoplanet_catalogue/master/'
+OEC_META_SERVER = 'https://raw.github.com/hannorein/oec_meta/master/'
+
+def change_to_local(filepath):
+    """(str) -> Notype
+    change the querying to use a local version of the open exoplanet catalogue.
+
+    filepath must be absolute.
+
+    change_to_local("/home/usrname/open_exoplanet_catalogue/")
+    >>>"Now using local data at /home/usrname/open_exoplanet_catalogue"
+    """
+    global OEC_SERVER
+    global OEC_META_SERVER
+    OEC_SERVER = "file://"+filepath
+    OEC_META_SERVER = "file://"+filepath
+
+    print("Now using local data at" + filepath)
+
+def change_to_remote():
+    """ Change the queries to use the default remote database"""
+    global OEC_SERVER
+    global OEC_META_SERVER
+    OEC_SERVER = 'https://raw.github.com/hannorein/open_exoplanet_catalogue/master/'
+    OEC_META_SERVER = 'https://raw.github.com/hannorein/oec_meta/master/'
+
+    print("Now using remote data")
+    print("Fetching data may be slow. Switch to local")
 
 def find_system_for_alias(alias):
     """ (str) -> str
@@ -46,7 +73,7 @@ def find_system_for_alias(alias):
     global aliases
     alias = alias.strip()
     if aliases is None:
-        metaserver = OEC_META_SERVER()
+        metaserver = OEC_META_SERVER
         metaurl = metaserver+"aliases.csv"
         try:
             metacsv = urllib2.urlopen(metaurl)
@@ -63,8 +90,11 @@ def get_xml_for_system(system,category='systems'):
     Returns the xml file as a str for the given system
     """
 
+    if(OEC_SERVER[:4] != "file"):
+        print("Using remote data, queries may be slow")
+
     try:
-        url = OEC_SERVER() + category + "/" +\
+        url = OEC_SERVER + category + "/" +\
                 urllib2.quote(system) + ".xml"
         xml = urllib2.urlopen(url).read()
     except TypeError:
@@ -86,21 +116,29 @@ def xml_element_to_dict(e):
     return d
     
 def query_system_xml(system_id,category='systems'):
-    """ (str, *str) -> System 
+    """ (str, *str) -> str 
     Queries the database and returns the XML data of the system 
     """
 
     system = find_system_for_alias(system_id)
     return get_xml_for_system(system,category)
 
-def query_system_to_obj(system_id, category='systems'):
+def query_system_dict(system_id, category='systems'):
+    """ (str, *str) -> dict 
+    Queries the database and returns the python dict of data of the system 
+    """
+
+    system = find_system_for_alias(system_id)
+    return xml_element_to_dict(get_xml_for_system(system,category))
+
+def query_system_obj(system_id, category='systems'):
     """ (str, *str) -> System 
     Queries the database and returns a System object of the system
     """
 
     return xml_to_obj(query_system_xml(system_id,category))
 
-def query_planet(planet_id,category='systems'):
+def query_planet_dict(planet_id,category='systems'):
     """ (str, *str) -> dict
     Queries the database and returns the planet data as a python dictionary 
     """
@@ -117,7 +155,25 @@ def query_planet(planet_id,category='systems'):
 
     return None
 
-def query_planet_to_obj(planet_id, category='systems'):
+def query_planet_xml(planet_id, category='systems'):
+    """ (str, *str) -> xml 
+    Queries the database and returns the planet data as a python dictionary 
+    """
+
+    system = find_system_for_alias(planet_id)
+    xml = get_xml_for_system(system,category)
+    systemroot = ET.fromstring(xml)
+    planets = systemroot.findall(".//planet")
+    for planet in planets:
+        names = planet.findall("./name")
+        for name in names:
+            if name.text == planet_id.strip():
+                return planet
+
+    return None
+
+
+def query_planet_obj(planet_id, category='systems'):
     """(str, *str) -> System
     Queries the database and returns the planet data as a System type
     """
@@ -129,3 +185,68 @@ def query_planet_to_obj(planet_id, category='systems'):
 
 
     return None
+
+def all_system_identifiers():
+    """(NoType) -> list
+    returns a list of strings of all of the unique systems
+    """
+
+    global aliases
+    if aliases is None:
+        metaserver = OEC_META_SERVER
+        metaurl = metaserver+"aliases.csv"
+        try:
+            metacsv = urllib2.urlopen(metaurl)
+            aliases = list(csv.reader(metacsv))
+        except:
+            raise CatalogueOpeningError("Error getting Open Exoplanet Catalogue file '"+metaurl+\
+			    "'.\nCheck system_id and server.")
+    systems = []
+    for sys in aliases:
+        if sys[2] == "system" and not (sys[1] in systems):
+            systems.append(sys[1])
+    return systems
+
+def all_planet_identifiers():
+    """(NoType) -> list
+    returns a list of strings of all of the unique planets 
+    """
+
+    global aliases
+    if aliases is None:
+        metaserver = OEC_META_SERVER
+        metaurl = metaserver+"aliases.csv"
+        try:
+            metacsv = urllib2.urlopen(metaurl)
+            aliases = list(csv.reader(metacsv))
+        except:
+            raise CatalogueOpeningError("Error getting Open Exoplanet Catalogue file '"+metaurl+\
+			    "'.\nCheck system_id and server.")
+    planets = []
+    for plan in aliases:
+        if plan[2] == "planet" and not(plan[3] in planets):
+            planets.append(plan[3])
+    return planets
+
+def all_star_identifiers():  
+    """(NoType) -> list
+    returns a list of strings of all of the unique stars 
+    """
+
+    global aliases
+    if aliases is None:
+        metaserver = OEC_META_SERVER
+        metaurl = metaserver+"aliases.csv"
+        try:
+            metacsv = urllib2.urlopen(metaurl)
+            aliases = list(csv.reader(metacsv))
+        except:
+            raise CatalogueOpeningError("Error getting Open Exoplanet Catalogue file '"+metaurl+\
+			    "'.\nCheck system_id and server.")
+    stars = []
+    for star in aliases:
+        if star[2] == "star" and not(star[3] in stars):
+            stars.append(star[3])
+    return stars
+
+
