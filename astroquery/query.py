cef03eba889f7bfdf7a0bae402969ce173d74a91
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2019-05-04 11:59:45 -0600

    more interactive tests revealed that my hacks didn't really work, this commit has some fixes

--- astroquery/query.py
+++ astroquery/query.py
@@ -267,7 +267,7 @@ class BaseQuery:
 
     def _download_file(self, url, local_filepath, timeout=None, auth=None,
                        continuation=True, cache=False, method="GET",
-                       head_safe=False, **kwargs):
+                       head_safe=False, close_response=True, **kwargs):
         """
         Download a file.  Resembles `astropy.utils.data.download_file` but uses
         the local ``_session``
@@ -385,7 +385,8 @@ class BaseQuery:
                     else:
                         pb.update(bytes_read)
 
-        response.close()
+        if close_response:
+            response.close()
         return response
 
 
